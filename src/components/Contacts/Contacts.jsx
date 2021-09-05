557cdf7a59a209723c7a945589235ed9b903098e
Anzhela Boiko <boikoanzhela@gmail.com>
2021-09-05 16:18:02 +0300

    convert conctacts to func

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,33 +1,32 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import s from "./Contacts.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Contacts.module.css';
 
-class Contacts extends Component {
-  handleClick = (id) => {
-    this.props.onClick(id);
+function Contacts({ contactsItem, onClick }) {
+  const handleClick = id => {
+    onClick(id);
   };
-  render() {
-    const contacts = this.props.contactsItem;
-    return (
-      <ul className={s.contactsList}>
-        {contacts.map((item) => (
-          <li key={item.id} className={s.contactsItem}>
-            <p className={s.contactsText}>
-              {item.name}: {item.number}
-            </p>
-            <button
-              className={s.contactsBtn}
-              type="button"
-              onClick={() => this.handleClick(item.id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+
+  return (
+    <ul className={s.contactsList}>
+      {contactsItem.map(item => (
+        <li key={item.id} className={s.contactsItem}>
+          <p className={s.contactsText}>
+            {item.name}: {item.number}
+          </p>
+          <button
+            className={s.contactsBtn}
+            type="button"
+            onClick={() => handleClick(item.id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
+
 export default Contacts;
 Contacts.propTypes = {
   onClick: PropTypes.func.isRequired,
